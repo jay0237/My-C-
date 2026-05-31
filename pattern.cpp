063059335cc14caa934efbfa8f356d214173ffed
jay0237 <jayjoshi16534@gmail.com>
2026-05-31 21:30:16 +0530

    Patterm

--- pattern.cpp
+++ pattern.cpp
@@ -59,28 +59,28 @@
 // //     return 0;
 // // }
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main() {
+// int main() {
 
-    int n;
+//     int n;
 
-    cout << "Enter a value: " << endl;
-    cin >> n;
+//     cout << "Enter a value: " << endl;
+//     cin >> n;
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if (i==0 || j==0 || i==n-1 || j==n-1){
-                cout << "* ";
-            }
-            else{
-                cout << "  ";
-            }
-        }
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             if (i==0 || j==0 || i==n-1 || j==n-1){
+//                 cout << "* ";
+//             }
+//             else{
+//                 cout << "  ";
+//             }
+//         }
 
-        cout << endl;
-    }
+//         cout << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
