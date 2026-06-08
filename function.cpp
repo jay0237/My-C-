15cc285272f16f909d5e100ee31fc1b4afa33a04
jay0237 <jayjoshi16534@gmail.com>
2026-06-08 12:54:02 +0530

    function

---
+++ function.cpp
@@ -0,0 +1,27 @@
+// #include<iostream>
+// using namespace std;
+
+// float sum( float a, float b){
+//     float  x = a + b;
+
+//     return x;
+// }
+
+// int main() {
+
+//     cout << sum (5,8.9) << endl;
+
+//     return 0;
+    
+// }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int  n;
+
+    cout << "Enter a number of N:v " << endl;
+    cin >> n;
+}
