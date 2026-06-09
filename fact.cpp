f894aa46613ac0834a329e6eaf365bcd6296fbf1
jay0237 <jayjoshi16534@gmail.com>
2026-06-09 16:10:21 +0530

    Factorial

---
+++ fact.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+
+//     int fact = 1;
+//     int n;
+
+//     cout << "Enter a number: " << endl;
+//     cin >> n;
+
+//     for (int i=1;i<=n;i++){
+//         fact = fact * i;
+//     }
+//     cout << "Factorial of " << n << " is: " << fact << endl;
+//     return 0;
+// }
+
