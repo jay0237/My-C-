c810ec79891edecfd996af14b182ec9ccff36122
jay0237 <jayjoshi16534@gmail.com>
2026-06-09 16:15:19 +0530

    Factorial with function

--- fact.cpp
+++ fact.cpp
@@ -16,3 +16,28 @@
 //     return 0;
 // }
 
+#include<iostream>
+using namespace std;
+
+double facto(int n){
+
+    double fact=1;
+    for (int i=1;i<=n;i++){
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+
+int main(){
+    int n;
+
+    cout << "Enter a number: "<< endl;
+    cin >> n;
+
+    cout << "Factorial: " << facto(n) << endl;
+
+    return 0;
+ 
+}
