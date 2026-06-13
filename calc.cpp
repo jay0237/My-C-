c2274c1db70a2eca43c5c5e0a49974fa2c523b4d
jay0237 <jayjoshi16534@gmail.com>
2026-06-13 18:13:53 +0530

    ..

--- calc.cpp
+++ calc.cpp
@@ -1,47 +1,44 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-
+int main(){
     int a;
     int b;
 
-    cout << "Enter a first number: " << endl;
+    cout << "Enter a number: " << endl;
     cin >> a;
 
     cout << "Enter a second number: " << endl;
     cin >> b;
 
     int sum = a+b;
-    int subtract = a-b;
-    int product = a*b;
+    int sub = a-b;
+    int mul =a*b;
     int div = a/b;
 
-    cout << "1.sum" << endl;
-    cout << "2.subtract" << endl;
-    cout << "3.product" << endl;
-    cout << "4.division" << endl;
-    cout << "Enter your choice: " << endl;
+    cout <<"1.Sum" << endl;
+    cout <<"2.Subtraction" << endl;
+    cout <<"3.Multiplication" << endl;
+    cout <<"4.Division" << endl;
+    cout <<"Enter your Choice: " <<endl;
 
     int choice;
     cin >> choice;
 
-    if(choice == 1){
-        cout << "The sum of two number is: " << sum << endl;
+    switch(choice){
+        case 1:
+            cout << "Sum: " << sum << endl;
+            break;
+        case 2:
+            cout << "Subtraction: " << sub << endl;
+            break;
+        case 3:
+            cout << "Multiplication: " << mul << endl;
+            break;
+        case 4:
+            cout << "Division: " << div << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
     }
-    else if(choice == 2){
-        cout << "The subtract of two number is: " << subtract << endl;
-    }
-    else if(choice == 3){
-        cout << "the product of two number is: " << product << endl;        
-    }
-    else if(choice == 4){
-        cout << "The division of two number is: " << div << endl;
-    }
-    else{
-        cout << "Invalid Choice" << endl;   
-    }
-    
-
-    return 0;
 }
--- fact.cpp
+++ fact.cpp
@@ -1,43 +1,51 @@
+// // #include<iostream>
+// // using namespace std;
+
+// // int main() {
+
+// //     int fact = 1;
+// //     int n;
+
+// //     cout << "Enter a number: " << endl;
+// //     cin >> n;
+
+// //     for (int i=1;i<=n;i++){
+// //         fact = fact * i;
+// //     }
+// //     cout << "Factorial of " << n << " is: " << fact << endl;
+// //     return 0;
+// // }
+
 // #include<iostream>
 // using namespace std;
 
-// int main() {
+// double facto(int n){
 
-//     int fact = 1;
-//     int n;
-
-//     cout << "Enter a number: " << endl;
-//     cin >> n;
-
+//     double fact=1;
 //     for (int i=1;i<=n;i++){
 //         fact = fact * i;
 //     }
-//     cout << "Factorial of " << n << " is: " << fact << endl;
-//     return 0;
+
+//     return fact;
 // }
+
+
+// int main(){
+//     int n;
+
+//     cout << "Enter a number: "<< endl;
+//     cin >> n;
+
+//     cout << "Factorial: " << facto(n) << endl;
+
+//     return 0;
+ 
+// }
+
 
 #include<iostream>
 using namespace std;
 
-double facto(int n){
-
-    double fact=1;
-    for (int i=1;i<=n;i++){
-        fact = fact * i;
-    }
-
-    return fact;
-}
-
-
 int main(){
-    int n;
 
-    cout << "Enter a number: "<< endl;
-    cin >> n;
-
-    cout << "Factorial: " << facto(n) << endl;
-
-    return 0;
- 
 }
